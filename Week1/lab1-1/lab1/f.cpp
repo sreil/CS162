c6ddab4d89aea1f3652a3436cfc92caf743a621a
sreil <seanpreilly@gmail.com>
2015-04-12 21:49:42 -0600

    added first assignment

---
+++ Week1/lab1-1/lab1/f.cpp
@@ -0,0 +1,7 @@
+#include "f.h"
+#include <iostream>
+
+void F::f()
+{
+	std::cout << "This was function f!" << std::endl;
+}
---
+++ Week1/lab1-1/lab1/g.cpp
@@ -0,0 +1,7 @@
+#include "g.h"
+#include <iostream>
+
+void G::g()
+{
+	std::cout << "This was function g!" << std::endl;
+}
